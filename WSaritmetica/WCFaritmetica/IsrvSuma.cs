6abc32d7bcaf606b2a9f204afeb90e557d008e09
Hugo Balcazar <hugobalcazar1509@gmail.com>
2019-02-23 13:56:30 -0400

    Agregamos unit test

--- WSaritmetica/WCFaritmetica/IsrvSuma.cs
+++ WSaritmetica/WCFaritmetica/IsrvSuma.cs
@@ -19,7 +19,7 @@ namespace WCFaritmetica
     }
 
     [DataContract]
-    public class clsSuma
+    public class clsSuma:baseResultado
     {
         [DataMember]
         public int numeroA { get; set; }
@@ -29,4 +29,9 @@ namespace WCFaritmetica
         public int resultado { get; set; }
     }
 
+    public class baseResultado
+    {
+        public string mensaje { get; set; }
+        public string error { get; set; }
+    }
 }
